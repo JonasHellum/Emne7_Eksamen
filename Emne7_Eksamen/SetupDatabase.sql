878fb6b1964ab756dc8b1d93259121453a195c33
Jonas Lærke Hellum <jonas_laerke@hotmail.com>
2025-01-07 19:58:38 +0100

    fixed a minor issue

--- Emne7_Eksamen/SetupDatabase.sql
+++ Emne7_Eksamen/SetupDatabase.sql
@@ -2,5 +2,8 @@ DROP DATABASE IF EXISTS gokstad_athletics;
 CREATE DATABASE gokstad_athletics;
 use gokstad_athletics;
 
+CREATE USER IF NOT EXISTS 'ga-app'@'localhost' IDENTIFIED BY 'ga-5ecret-%';
+CREATE USER IF NOT EXISTS 'ga-app'@'%' IDENTIFIED BY 'ga-5ecret-%';
+
 GRANT ALL privileges ON gokstad_athletics.* TO 'ga-app'@'%';
 GRANT ALL privileges ON gokstad_athletics.* TO 'ga-app'@'localhost';
